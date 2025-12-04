77522e8b2e30ce643eda735505ffa412bb91886c
Kafka2306 <kafka@example.com>
2025-12-05 01:28:01 +0900

    feat: add dynamic SEO meta tags for game pages (issue #49)

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HelmetProvider } from 'react-helmet-async'
 import App from './App.jsx'
 import DataViewer from './DataViewer.jsx'
 import GamePage from './pages/GamePage.jsx'
@@ -8,12 +9,14 @@ import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/data" element={<DataViewer />} />
-        <Route path="/games/:slug" element={<GamePage />} />
-      </Routes>
-    </BrowserRouter>
+    <HelmetProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/data" element={<DataViewer />} />
+          <Route path="/games/:slug" element={<GamePage />} />
+        </Routes>
+      </BrowserRouter>
+    </HelmetProvider>
   </StrictMode>
 )
