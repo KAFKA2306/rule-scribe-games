146f1573b57c2101f6bf712f95f93017a0c94c5a
Kafka2306 <kafka@example.com>
2025-12-12 06:17:45 +0900

    fix: lint errors in main.py and index.py

--- api/index.py
+++ api/index.py
@@ -5,4 +5,4 @@ import os
 root_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(root_path)
 
-from app.main import app
+from app.main import app  # noqa: E402, F401
